a56e65f52765cf465c6911224a9d449dd8c83fd6
Voldy87 <ing.orlandi.andrea@gmail.com>
2017-12-05 09:32:40 +0100

    fixed git tracking

--- loadLocalhostDBinitialData.bat
+++
@@ -1,9 +0,0 @@
-cd /d D:/andre/mongo/
-start mongod --dbpath data
-cd /d mongomart/initialDbData/
-timeout /t 10
-for /r %%i in (*.json) do (
-mongoimport --drop -d unrealmart -c  %%~ni %%i
-)
-exit
-
